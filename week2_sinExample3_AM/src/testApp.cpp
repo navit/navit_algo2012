f76e49121f4da583a25487229de1857aecf4a422
navit <navitush@gmail.com>
2012-09-25 23:53:22 -0400

    second week of home work

---
+++ week2_sinExample3_AM/src/testApp.cpp
@@ -0,0 +1,90 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	
+	ofSetVerticalSync(true);
+	
+	ofBackground(0,0,0);
+	
+	//ofSetCircleResolution(100);
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	
+	
+	
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	
+    // regulates the carrier change its behaviour  To vary the frequency, amplitude, phase, or other characteristic
+	float modulator = ofMap( sin(ofGetElapsedTimef()*10), -1, 1, 0, 1); //mapping -1 & 1 to 0 & 1
+	
+    //carriers
+    float ypos= ofMap( cos(ofGetElapsedTimef()*50) * modulator, -1, 1, 5, ofGetHeight()-20);
+	float xpos= ofMap( sin (ofGetElapsedTimef()*50) * modulator, -1, 1, 5, ofGetWidth()-50);
+	
+    ofSetColor(64,255,64);
+	ofCircle(xpos, ypos,4 );
+    
+    
+    //drawing and storing the points
+    ofPoint temp;
+	temp.x = xpos;
+	temp.y = ypos;
+	points.push_back(temp);
+	if (points.size() > 1000){//how many points to store
+		points.erase(points.begin());
+	}
+    
+    ofBeginShape();
+	for (int i = 0; i < points.size(); i++){
+		ofVertex(points[i].x, points[i].y);
+	}
+	ofEndShape();
+	
+	
+	
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
