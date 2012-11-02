e0e28641058753ce463e578a1c7ef3e5afd870e7
navit <navitush@gmail.com>
2012-11-01 23:16:13 -0400

    image and particles

---
+++ week7_particle_w_image/src/particle.cpp
@@ -0,0 +1,255 @@
+#include "particle.h"
+#include "ofMain.h"
+
+//------------------------------------------------------------
+particle::particle(){
+	setInitialCondition(0,0,0,0);
+	damping = 0.12f;
+    //lead =5;
+}
+
+//------------------------------------------------------------
+void particle::resetForce(){
+    // we reset the forces every frame
+    frc.set(0,0);
+}
+
+//------------------------------------------------------------
+void particle::addForce(float x, float y){
+    // add in a force in X and Y for this frame.
+    frc.x = frc.x + x;
+    frc.y = frc.y + y;
+}
+
+//------------------------------------------------------------
+void particle::addRepulsionForce(float x, float y, float radius, float scale){
+    
+	// ----------- (1) make a vector of where this position is: 
+	
+	ofVec2f posOfForce;
+	posOfForce.set(x,y);
+	
+	// ----------- (2) calculate the difference & length 
+	
+	ofVec2f diff	= pos - posOfForce;
+	float length	= diff.length();
+	
+	// ----------- (3) check close enough
+	
+	bool bAmCloseEnough = true;
+    if (radius > 0){
+        if (length > radius){
+            bAmCloseEnough = false;
+        }
+    }	
+	// ----------- (4) if so, update force
+    
+	if (bAmCloseEnough == true){
+		float pct = 1 - (length / radius);  // stronger on the inside
+        diff.normalize();
+		frc.x = frc.x + diff.x * scale * pct;
+        frc.y = frc.y + diff.y * scale * pct;
+    }
+}
+
+//------------------------------------------------------------
+void particle::addAttractionForce(float x, float y, float radius, float scale){
+    
+	// ----------- (1) make a vector of where this position is: 
+	
+	ofVec2f posOfForce;
+	posOfForce.set(x,y);
+	
+	// ----------- (2) calculate the difference & length 
+	
+	ofVec2f diff	= pos - posOfForce;
+	float length	= diff.length();
+	
+	// ----------- (3) check close enough
+	
+	bool bAmCloseEnough = true;
+    if (radius > 0){
+        if (length > radius){
+            bAmCloseEnough = false;
+        }
+    }
+	
+	// ----------- (4) if so, update force
+    
+	if (bAmCloseEnough == true){
+		float pct = 1 - (length / radius);  // stronger on the inside
+		diff.normalize();
+		frc.x = frc.x - diff.x * scale * pct;
+        frc.y = frc.y - diff.y * scale * pct;
+    }
+}
+
+//------------------------------------------------------------
+void particle::addRepulsionForce(particle &p, float radius, float scale){
+	
+	// ----------- (1) make a vector of where this particle p is: 
+	ofVec2f posOfForce;
+	posOfForce.set(p.pos.x,p.pos.y);
+	
+	// ----------- (2) calculate the difference & length 
+	
+	ofVec2f diff	= pos - posOfForce;
+	float length	= diff.length();
+	
+	// ----------- (3) check close enough
+	
+	bool bAmCloseEnough = true;
+    if (radius > 0){
+        if (length > radius){
+            bAmCloseEnough = false;
+        }
+    }
+	
+	// ----------- (4) if so, update force
+    
+	if (bAmCloseEnough == true){
+		float pct = 1 - (length / radius);  // stronger on the inside
+		diff.normalize();
+					frc.x = frc.x + diff.x * scale * pct;
+        frc.y = frc.y + diff.y * scale * pct;
+					p.frc.x = p.frc.x - diff.x * scale * pct;
+        p.frc.y = p.frc.y - diff.y * scale * pct;
+    }
+}
+
+//------------------------------------------------------------
+void particle::addAttractionForce(particle & p, float radius, float scale){
+	
+	// ----------- (1) make a vector of where this particle p is: 
+	ofVec2f posOfForce;
+	posOfForce.set(p.pos.x,p.pos.y);
+	
+	// ----------- (2) calculate the difference & length 
+	
+	ofVec2f diff	= pos - posOfForce;
+	float length	= diff.length();
+	
+	// ----------- (3) check close enough
+	
+	bool bAmCloseEnough = true;
+    if (radius > 0){
+        if (length > radius){
+            bAmCloseEnough = false;
+        }
+    }
+	
+	// ----------- (4) if so, update force
+    
+	if (bAmCloseEnough == true){
+		float pct = 1 - (length / radius);  // stronger on the inside
+		diff.normalize();
+		frc.x = frc.x - diff.x * scale * pct;
+        frc.y = frc.y - diff.y * scale * pct;
+		p.frc.x = p.frc.x + diff.x * scale * pct;
+        p.frc.y = p.frc.y + diff.y * scale * pct;
+    }
+	
+}
+
+
+//------------------------------------------------------------
+void particle::addDampingForce(){
+	
+	// the usual way to write this is  vel *= 0.99
+	// basically, subtract some part of the velocity 
+	// damping is a force operating in the oposite direction of the 
+	// velocity vector
+	
+    frc.x = frc.x - vel.x * damping;
+    frc.y = frc.y - vel.y * damping;
+}
+
+//------------------------------------------------------------
+void particle::setInitialCondition(float px, float py, float vx, float vy){
+    pos.set(px,py);
+	vel.set(vx,vy);
+    
+    //drawing variables
+    float angle = atan2(pos.y-ofGetHeight()/2,pos.x-ofGetWidth()/2)+PI;
+    float distance = ofDist(pos.x,pos.y,ofGetWidth()/2,ofGetHeight()/2);
+    float sat = ofMap(distance,0,ofGetWidth(),150,200,true);
+    float brightness = ofMap(distance,ofGetWidth(),ofGetWidth(),180,100,true);
+    color = ofColor::fromHsb(sat,brightness,255);
+}
+
+//------------------------------------------------------------
+void particle::update(){	
+	vel = vel + frc;
+	pos = pos + vel;
+    
+    if(trail.size()>100);{
+        trail.push_back(pos);
+    }
+}
+
+//------------------------------------------------------------
+void particle::draw(){
+    //ofCircle(pos.x, pos.y, 3);
+	img->draw(pos.x, pos.y, 150, 150);
+  
+    ofSetColor(200,200);
+	ofPoint temp;
+	temp.x = pos.x;
+	temp.y = pos.y;
+	trail.push_back(temp);
+	if (trail.size() > 100){
+		trail.erase(trail.begin());
+	}
+    
+    ofSetColor(color,15);
+    ofSetLineWidth(2);
+    ofNoFill();
+    ofMesh lineMesh;
+	ofBeginShape();
+	for (int i = 0; i < trail.size(); i++){
+        lineMesh.addVertex(trail[i]);
+        ofVertex(trail[i].x, trail[i].y);
+	}
+	ofEndShape();
+    
+}
+
+
+//------------------------------------------------------------
+void particle::bounceOffWalls(){
+	
+	// sometimes it makes sense to damped, when we hit
+	bool bDampedOnCollision = true;
+	bool bDidICollide = false;
+	
+	// what are the walls
+	float minx = 0;
+	float miny = 0;
+	float maxx = ofGetWidth();
+	float maxy = ofGetHeight();
+	
+	if (pos.x > maxx){
+		pos.x = maxx; // move to the edge, (important!)
+		vel.x *= -1;
+		bDidICollide = true;
+	} else if (pos.x < minx){
+		pos.x = minx; // move to the edge, (important!)
+		vel.x *= -1;
+		bDidICollide = true;
+	}
+	
+	if (pos.y > maxy){
+		pos.y = maxy; // move to the edge, (important!)
+		vel.y *= -1;
+		bDidICollide = true;
+	} else if (pos.y < miny){
+		pos.y = miny; // move to the edge, (important!)
+		vel.y *= -1;
+		bDidICollide = true;
+	}
+	
+	if (bDidICollide == true && bDampedOnCollision == true){
+		vel *= 0.3;
+	}
+	
+}
