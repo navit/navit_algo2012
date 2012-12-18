9361cc4660c1aa0605d7dafd7a86b47408085831
navit <navitush@gmail.com>
2012-12-18 00:20:13 -0500

    more development on my bacteria growth

---
+++ FinalV1/src/testApp.cpp
@@ -0,0 +1,167 @@
+#include "testApp.h"
+
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    //ofSetBackgroundAuto(false);
+    ofBackground(0,0,0);
+	ofSetVerticalSync(true);
+	ofSetFrameRate(20);
+	//n=1;
+    
+	bRepel		= true;
+	radius		= 40;
+	strength	= 0.5f;
+    
+    life = 1;
+    death = 0;
+    max_bacteria = 100;
+    timer =0;
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    
+
+    if (timer%30==0){
+    
+        {
+                   for (int y = 0; y < life; y++){
+                        particle myParticle;
+                        myParticle.setInitialCondition(ofRandom(ofGetWidth()/2-40,ofGetWidth()/2+40),ofRandom(ofGetHeight()/2-40,ofGetHeight()/2+40),0,0);
+                        particles.push_back(myParticle);
+                    }
+               }
+            
+             }
+
+    
+    
+    
+    //    n++;
+//    
+//    if(n<10)
+//    {
+//        for (int y = 0; y < n; y++){
+//            particle myParticle;
+//            myParticle.setInitialCondition(ofRandom(ofGetWidth()/2-40,ofGetWidth()/2+40),ofRandom(ofGetHeight()/2-40,ofGetHeight()/2+40),0,0);
+//            particles.push_back(myParticle);
+//        }
+//    }
+//    else{
+//        n=0;
+//       
+//    }
+//    
+
+	for (int i = 0; i < particles.size(); i++){
+		particles[i].resetForce();
+	}
+	
+	
+	for (int i = 0; i < particles.size(); i++){
+		
+		particles[i].addRepulsionForce(mouseX, mouseY, 200, 0.02);//the bigger the number the bigger the repulsion.
+		
+		for (int j = 0; j < i; j++){
+			particles[i].addRepulsionForce(particles[j], 50, 0.4);
+			particles[i].addAttractionForce(particles[j], 500, 0.005);
+		}
+		for (int j = 0; j < i; j++){
+			if (bRepel){
+				particles[i].addRepulsionForce(particles[j], radius, strength);
+			} else {
+				particles[i].addAttractionForce(particles[j], radius, strength);
+			}		}
+	}
+	
+	for (int i = 0; i < particles.size(); i++){
+		particles[i].addDampingForce();
+		particles[i].update();
+	}
+    
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+	ofEnableAlphaBlending();
+    	
+	
+	for (int i = 0; i < particles.size(); i++){
+		particles[i].draw();
+	}
+	
+	string reportString =	"(space) = reset\n(a/s) strength = " + ofToString(strength) + 
+							"\n(z/x) radius = " + ofToString(radius) + 
+							"\n(r) toggle mode = " + (bRepel ? "repelling" : "attracting");
+	
+	ofSetColor(40,40,0);
+	ofRect(10,10,300,80);
+	ofSetColor(255,255,255);
+	ofDrawBitmapString(reportString, 30, 30);
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){ 
+	
+	switch (key){
+			
+		case ' ':
+			// reposition everything: 
+			for (int i = 0; i < particles.size(); i++){
+				particles[i].setInitialCondition(ofRandom(0,ofGetWidth()),ofRandom(0,ofGetHeight()),0,0);
+			}
+			break;
+		case 'a':
+			strength += 0.02f;
+			break;
+		case 's':
+			strength -= 0.02f;
+			if (strength <= 0) strength = 0.02f;
+			break;
+		case 'z':
+			radius += 5.0f;
+			break;
+		case 'x':
+			radius -= 5.0f;
+			if (radius <= 0) radius = 5.0f;
+			break;
+		case 'r':
+			bRepel = !bRepel;
+			break;
+	}
+	
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased  (int key){ 
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+//	particles.erase(particles.begin());
+//	particle myParticle;
+//	myParticle.setInitialCondition(x,y,0,0);
+//	particles.push_back(myParticle);
+    
+ 
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+    particles.clear();
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(){
+
+}
